cf874226780ed62fcfd3715f3da053f103fa82d9
Addison Moore <addisondrewmoore@gmail.com>
2018-10-27 12:04:29 -0700

    added game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
 let cardsInPlay = [];
 
 function checkForMatch() {
@@ -13,10 +34,11 @@ function checkForMatch() {
 }
 
 function flipCard(cardId) {
-  console.log(`User flipped a ${cardId}!`);
-  cardsInPlay.push(cardId);
+  console.log(`User flipped a ${cardId.rank} of ${cardId.suit}!`);
+  console.log(cardId.cardImage);
+  cardsInPlay.push(cardId.rank);
   checkForMatch();
 }
 
 flipCard(cards[1]);
-flipCard(cards[3]);
+flipCard(cards[2]);
