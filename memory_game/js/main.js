2f536175a2db9ec2beb09497c770d1232e8168f1
Addison Moore <addisondrewmoore@gmail.com>
2018-10-26 23:44:59 -0700

    added game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,22 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log(`User flipped ${cardOne}`);
-
-let cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log(`User flipped ${cardTwo}`);
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.");
+function checkForMatch() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again.");
+    }
+    cardsInPlay = [];
   }
 }
+
+function flipCard(cardId) {
+  console.log(`User flipped a ${cardId}!`);
+  cardsInPlay.push(cardId);
+  checkForMatch();
+}
+
+flipCard(cards[1]);
+flipCard(cards[3]);
